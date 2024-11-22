d8038aef847bfcbf38d07277dfdb026099948625
Yonder Blue <fields@yonder.blue>
2024-11-22 17:48:00 -0500

    initial contents (#1)

---
+++ dynamicpool_test.go
@@ -0,0 +1,74 @@
+package bytepool_test
+
+// originally from https://github.com/valyala/bytebufferpool/blob/master/pool_test.go
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/grax/bytepool"
+)
+
+func TestPoolCalibrate(t *testing.T) {
+	p := bytepool.NewDynamic()
+	for i := 0; i < 20*42000; i++ { // steps and calibrateCallsThreshold
+		n := 1004
+		if i%15 == 0 {
+			n = rand.Intn(15234) //nolint:gosec
+		}
+		testGetPut(t, p, n)
+	}
+}
+
+func TestPoolVariousSizesSerial(t *testing.T) {
+	testPoolVariousSizes(t)
+}
+
+func TestPoolVariousSizesConcurrent(t *testing.T) {
+	concurrency := 5
+	ch := make(chan struct{})
+	for i := 0; i < concurrency; i++ {
+		go func() {
+			testPoolVariousSizes(t)
+			ch <- struct{}{}
+		}()
+	}
+	for i := 0; i < concurrency; i++ {
+		select {
+		case <-ch:
+		case <-time.After(3 * time.Second):
+			t.Fatalf("timeout")
+		}
+	}
+}
+
+func testPoolVariousSizes(t *testing.T) {
+	p := bytepool.NewDynamic()
+	for i := 0; i < 20+1; i++ { // steps
+		n := (1 << uint32(i))
+
+		testGetPut(t, p, n)
+		testGetPut(t, p, n+1)
+		testGetPut(t, p, n-1)
+
+		for j := 0; j < 10; j++ {
+			testGetPut(t, p, j+n)
+		}
+	}
+}
+
+func testGetPut(t *testing.T, p bytepool.Pooler, n int) {
+	b := p.Get()
+	diffFatal(t, 0, len(b.B))
+	b.B = allocNBytes(b.B, n)
+	p.Put(b)
+}
+
+func allocNBytes(dst []byte, n int) []byte {
+	diff := n - cap(dst)
+	if diff <= 0 {
+		return dst[:n]
+	}
+	return append(dst, make([]byte, diff)...)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/grax/bytepool
+
+go 1.23
+
+require github.com/google/go-cmp v0.6.0
